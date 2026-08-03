2b551cbbb95dfb0509c7d12da5bf9a1e889f967a
Kineo CEO <josephsskaf@gmail.com>
2026-08-03 14:14:55 -0300

    CHORE: 27-PUSH.bat (og card + pesquisa concorrentes)

--- scripts/27-PUSH.bat
+++ scripts/27-PUSH.bat
@@ -1,8 +1,7 @@
 @echo off
-REM 27-PUSH - 03/08/2026 - sprint 13h (substitui o 25-PUSH, conteudo incluso)
+REM 27-PUSH - 03/08 tarde - OG CARD CONSERTADO (posts no X saem limpos)
 REM
-REM   603cced  PESQUISA Crayo/AutoShorts (ordens G1-G5)
-REM   7b957c0  G1: pagina EARN /make-money-clipping-with-ai + sitemap  |  G3: claim 40% na /partners
-REM   + docs sprints 12h/13h (placar, Fazier 48/#1, video_ready validado)
+REM   + pesquisa de concorrentes (ordens G1-G5) e docs das redes de protecao
 REM
+REM RODAR AGORA: seus proximos posts no X ja saem com o card certo.
 call "%~dp0\1-PUSH.bat"
