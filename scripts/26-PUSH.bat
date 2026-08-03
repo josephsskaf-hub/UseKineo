83c5d6767ec2ab528bb5afcb911ce557e3af4d88
Kineo CEO <josephsskaf@gmail.com>
2026-08-03 12:23:20 -0300

    CHORE: 26-PUSH.bat (banner PH)

---
+++ scripts/26-PUSH.bat
@@ -0,0 +1,8 @@
+@echo off
+REM 26-PUSH - 04/08/2026 00:2x - ULTIMO ANTES DO PH LAUNCH (00:01 PT / 04:00 BRT)
+REM
+REM   Banner de boas-vindas para visitantes do Product Hunt na home
+REM   (so aparece com utm/ref do PH; invisivel para o resto do trafego)
+REM
+REM RODAR ANTES DE DORMIR - o launch abre em ~3h30 e o banner precisa estar la.
+call "%~dp0\1-PUSH.bat"
